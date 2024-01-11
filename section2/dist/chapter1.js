7f8fe2edeab26e13db815f43a9e5fb9a659a6e65
monam2 <kangcw0107@gmail.com>
2024-01-11 21:59:00 +0900

    ts

---
+++ section2/dist/chapter1.js
@@ -0,0 +1,13 @@
+// let num1:number = 123;
+// let num2:number = -123;
+// let num3:number = 0.123;
+// let num4:number = Infinity;
+// let num5:number = -Infinity;
+// let num6:number = NaN;
+// console.log(num1);
+let str1 = "hello";
+let str2 = 'hello';
+let str3 = `hello`;
+let str4 = `hello ${str1}`;
+console.log(str4);
+export {};
