56d2811b70dc129b7fc365358c94f22d004043a3
monam2 <kangcw0107@gmail.com>
2024-01-16 21:47:18 +0900

    sec2

--- section2/dist/chapter1.js
+++ section2/dist/chapter1.js
@@ -5,9 +5,15 @@
 // let num5:number = -Infinity;
 // let num6:number = NaN;
 // console.log(num1);
-let str1 = "hello";
-let str2 = 'hello';
-let str3 = `hello`;
-let str4 = `hello ${str1}`;
-console.log(str4);
+// let str1:String = "hello";
+// let str2:String = 'hello';
+// let str3:String = `hello`;
+// let str4:String = `hello ${str1}`;
+//boolean
+let bool1 = true;
+let bool2 = false;
+// console.log(str4);
+let null1 = null;
+let unde1 = undefined;
 export {};
+// let numA:number = null; 불가
--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -7,9 +7,19 @@
 
 // console.log(num1);
 
-let str1:String = "hello";
-let str2:String = 'hello';
-let str3:String = `hello`;
-let str4:String = `hello ${str1}`;
+// let str1:String = "hello";
+// let str2:String = 'hello';
+// let str3:String = `hello`;
+// let str4:String = `hello ${str1}`;
 
-console.log(str4);
+//boolean
+let bool1:boolean = true;
+let bool2:boolean = false;
+
+// console.log(str4);
+
+let null1:null = null;
+
+let unde1 : undefined = undefined;
+
+// let numA:number = null; 불가
